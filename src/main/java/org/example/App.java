19267e2f61f4a58f1401fe90da5327bab4c2a135
maksJazzTeam <maksim.klyuev.jazzteam@gmail.com>
2023-10-24 21:59:18 +0300

    jte template with inputs, without css

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.template.JavalinJte;
 
 class App {
     public static void main(String[] args) {
         JavalinJte.init();
-        Javalin app = Javalin.create().start(7070);
+        Javalin app = Javalin.create(javalinConfig -> {
+            javalinConfig.staticFiles.add("/public");
+        }).start(7070);
         app.get("/", ctx -> ctx.render("index.jte"));
     }
 }

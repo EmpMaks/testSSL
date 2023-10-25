5563992e06ec2f619a78646c9182893f31a3a334
maksJazzTeam <maksim.klyuev.jazzteam@gmail.com>
2023-10-25 21:07:15 +0300

    server gets client input

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,5 +10,10 @@ class App {
             javalinConfig.staticFiles.add("/static");
         }).start(7070);
         app.get("/", ctx -> ctx.render("index.jte"));
+        app.post("/", context -> {
+            String ip = context.formParam("ip");
+            String threads = context.formParam("threads");
+            System.out.println(ip + "\n" +threads);
+        });
     }
 }
